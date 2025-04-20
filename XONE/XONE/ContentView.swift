6222208a9f87b49362c05a97d03bdd8c113ab143
XONE-Dev <>
2025-04-21 01:39:51 +0800

    update

---
+++ XONE/XONE/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  XONE
+//
+//  Created by XONE on 2025/4/21.
+//
+        
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XONE/XONE/XONEApp.swift
@@ -0,0 +1,18 @@
+//
+//  XONEApp.swift
+//  XONE
+//
+//  Created by XONE on 2025/4/21.
+//
+        
+
+import SwiftUI
+
+@main
+struct XONEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
